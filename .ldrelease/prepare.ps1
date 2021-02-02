5c4c9c36e3d2834a487d2afbae62cd71f7b544d3
Eli Bishop <eli@launchdarkly.com>
2021-02-02 14:38:43 -0800

    fix install command

--- .ldrelease/prepare.ps1
+++ .ldrelease/prepare.ps1
@@ -2,4 +2,4 @@
 # Extra pre-build step to install a newer .NET SDK than what CircleCI has,
 # because we need to be able to build for .NET Core 3.1.
 
-& "./dotnet-install.ps1" -Channel LTS
+& "./.ldrelease/dotnet-install.ps1" -Channel 5.0
