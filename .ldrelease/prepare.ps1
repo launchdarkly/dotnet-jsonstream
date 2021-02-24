4484adae004e16430c284d362fcf3aa0755dccce
Eli Bishop <eli@launchdarkly.com>
2021-02-24 14:51:02 -0800

    can now build .NET 5 in CircleCI Windows

--- .ldrelease/prepare.ps1
+++
@@ -1,6 +0,0 @@
-
-# Extra pre-build step to install a newer .NET SDK than what CircleCI has,
-# because we need to be able to build for .NET Core 3.1 and .NET 5.
-
-& "./.ldrelease/dotnet-install.ps1" -Channel 5.0
-[Environment]::SetEnvironmentVariable("Path", "C:\Users\circleci\AppData\Local\Microsoft\dotnet\;$env:Path", [System.EnvironmentVariableTarget]::Machine)
